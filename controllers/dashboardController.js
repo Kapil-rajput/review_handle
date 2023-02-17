ed0be29fa58fdbc6ca9c485145044cd92043e397
Kapil-rajput <kapil.bholu@gmail.com>
2023-02-17 13:26:19 +0530

    update

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,7 +1,8 @@
-
-
-module.exports.dashboard = (req, res) => {
+const Users = require("../models/user");
+module.exports.dashboard = async (req, res) => {
+  const admins = await Users.find();
   res.render("dashboard", {
-    user: req.user // Pass the user object to the view
+    user: req.user, // Pass the user object to the view
+    Admins: admins,
   });
 };
