770f15e03ea5d53adb898f9a0d3d5e1ec21cea35
Kapil-rajput <kapil.bholu@gmail.com>
2023-02-17 16:03:12 +0530

    major update

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -6,3 +6,32 @@ module.exports.dashboard = async (req, res) => {
     Admins: admins,
   });
 };
+
+module.exports.addEmployee = async (req, res) => {
+  const admins = await Users.find();
+  res.render("addEmployee", {
+    user: req.user, // Pass the user object to the view
+    Admins: admins,
+  });
+};
+module.exports.add = async (req, res) => {
+  const admins = await Users.find();
+  res.render("addEmp", {
+    user: req.user, // Pass the user object to the view
+    Admins: admins,
+  });
+};
+module.exports.edit = async (req, res) => {
+  const admins = await Users.find();
+  res.render("editEmp", {
+    user: req.user, // Pass the user object to the view
+    Admins: admins,
+  });
+};
+module.exports.delete = async (req, res) => {
+  const admins = await Users.find();
+  res.render("deleteEmp", {
+    user: req.user, // Pass the user object to the view
+    Admins: admins,
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const dashboardController = require('../controllers/dashboardController')
+const dashboardController = require('../controllers/dashboardController');
+const adminController = require('../controllers/adminController');
 const { initializingPassport, isAuthenticated } = require("../config/passport");
 
 
@@ -10,7 +11,11 @@ router.get("/", (req, res) => res.render("home"));
 router.get("/login", (req, res) => res.render("login"));
 router.get("/register", (req, res) => res.render("register"));
 router.get("/dashboard", isAuthenticated, dashboardController.dashboard);
-
+//this below one is only fro add update removve page not to add employee
+router.get("/addemployee", isAuthenticated, dashboardController.addEmployee);
+router.get("/add", isAuthenticated, dashboardController.add);
+router.get("/delete", isAuthenticated, dashboardController.delete);
+router.get("/edit", isAuthenticated, dashboardController.edit);
 
 
 
